bd7f74e9455d123a2efa0ba6dfdf0707bb2cee98
Kayo <cmml1@aluno.ifal.edu.br>
2026-07-28 11:35:23 -0300

    criação catalogo simples

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public abstract class Catalogo {
+        String[] armazem = {"Harry Potter 1", "Harry Potter 2", "Harry Potter 4", "Harry Potter 6", "Five Nights at Freddys"};
+        Scanner input = new Scanner(System.in);
+        
+        public void realizarBusca() {
+            
+            System.out.println("Pesquisar oq?");
+            
+            String pesquisa = input.next();
+
+            for (int i = 0; i < armazem.length; i++) {
+                if (armazem[i].contains(pesquisa)) {
+                    System.out.println(armazem[i]);
+                }
+            }
+        }
     }
 }
