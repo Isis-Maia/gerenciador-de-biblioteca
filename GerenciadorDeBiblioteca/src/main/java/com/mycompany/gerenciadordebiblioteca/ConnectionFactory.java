7333a6a3e04dc794c58973525ad09ad04db1b2d7
Guilherme Henrique <gh8331502@gmail.com>
2026-08-01 08:38:29 -0300

    add ConnectionFactory

--- GerenciadorDeBiblioteca/src/main/java/com/mycompany/gerenciadordebiblioteca/ConnectionFactory.java
+++ GerenciadorDeBiblioteca/src/main/java/com/mycompany/gerenciadordebiblioteca/ConnectionFactory.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = ""; 
     
     public static Connection getConnection(){
         try{
